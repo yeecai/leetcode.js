6a156667d36fcb4023e8d0f8317762e63fe5508d
Ye Cai <yushunchn@gmail.com>
2020-05-17 13:08:17 +0800

    Update and rename 105PreorderInorder.js to 105&106buildTrees.js

--- 1xxBinaryTree/105&106buildTrees.js
+++ 1xxBinaryTree/105&106buildTrees.js
@@ -19,3 +19,16 @@ var buildTree = function (preorder, inorder) {
     }
     return build(inorder)
 };
+
+//106
+var buildTree = function(inorder, postorder) {
+    let build = (inorder) => {
+        if(!inorder || !inorder.length) return null
+        let tmp = postorder.pop(), mid = inorder.indexOf(tmp)
+        let root = new TreeNode(tmp)
+        root.right = build(inorder.slice(mid+1))
+        root.left = build(inorder.slice(0, mid))
+        return root
+    }
+    return build(inorder)
+};

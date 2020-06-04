3679c3de627d0d161a5b3121feae8afa33b970b9
Ye Cai <yushunchn@gmail.com>
2020-06-04 12:16:21 +0800

    Create 1160charswords.js

---
+++ 1160charswords.js
@@ -0,0 +1,31 @@
+// version1.0 map
+var countCharacters = function (words, chars) {
+    // charsMap and wordMap
+    // compare each char of word with chars
+    // if word has a char not in charsMap or more than the char in charsMap , out 
+    let charsMap = new Map()
+    for (let char of chars) {
+        charsMap.set(char, (charsMap.has(char) ? charsMap.get(char) + 1 : 1))
+    }
+
+    let size = 0
+    for (let word of words) {
+        let wordMap = new Map()
+        for (let char of word) {
+            wordMap.set(char, (wordMap.has(char) ? wordMap.get(char) + 1 : 1))
+        }
+        let out = false
+        //compare
+        for (let char of word) {
+            if (wordMap.get(char) > charsMap.get(char) || charsMap.get(char) === undefined) {
+                out = true
+                break
+            }
+        }
+
+        if (out === false) {
+            size += word.length
+        }
+    }
+    return size
+};
